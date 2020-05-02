ddc0ab3b7647e70cf3723a5d306c74989ea58ece
David Gulbrandsen <david.gulbrandsen@gmail.com>
2020-05-02 17:06:03 +0200

    Update rules.mk

--- keyboards/Plancked/rules.mk
+++ keyboards/Plancked/rules.mk
@@ -41,5 +41,5 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
-TAP_DANCE_ENABLE= yes
-COMBO_ENABLE = yes
+#TAP_DANCE_ENABLE= yes
+#COMBO_ENABLE = yes
